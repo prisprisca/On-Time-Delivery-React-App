8fe9b390ff7082133bde80581dcc6c86c2350193
prisprisca <prisca20bamwireku@gmail.com>
2021-06-20 01:34:43 +0300

    add hosted url to the idex.js file

--- src/actions/orders.js
+++ src/actions/orders.js
@@ -4,6 +4,7 @@ import * as api from "../api";
 export const getOrders = () => async (dispatch) => {
   try {
     const { data } = await api.fetchOrders();
+    console.log(data);
     dispatch({ type: "FETCH_ALL", payload: data });
   } catch (error) {
     console.log(error.message);
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-import Order from './Order/Order';
-import useStyles from './styles';
-import { CircularProgress, Grid } from '@material-ui/core';
+import React from "react";
+import { useSelector } from "react-redux";
+import Order from "./Order/Order";
+import useStyles from "./styles";
+import { CircularProgress, Grid } from "@material-ui/core";
 
-const Orders = ({setCurrentId}) => {
-    const orders = useSelector((state)=> state.orders)
-    const classes = useStyles();
-    
-    return (
-        !orders.length ? <CircularProgress /> : (
-            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-            {orders.map((order) => (
-                <Grid key={order._id} item xs={12} sm={6}>
-                    <Order order={order} setCurrentId={setCurrentId} />
-                </Grid>
-            ))}
-                
+const Orders = ({ setCurrentId }) => {
+  const orders = useSelector((state) => state.orders);
+  const classes = useStyles();
+  console.log(orders);
 
-            </Grid>
-        )
-      
-    )
-}
+  // return (
+  //   <h1>order</h1>
+  // )
 
-export default Orders
+  return !orders.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {orders.map((order) => (
+        <Grid key={order._id} item xs={12} sm={6}>
+          <Order order={order} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default Orders;
