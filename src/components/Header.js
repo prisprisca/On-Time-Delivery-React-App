6bc90360897bb61613f7ae4ac76f94e184e1a220
prisprisca <prisca20bamwireku@gmail.com>
2021-06-21 10:43:36 +0300

    clear content in the header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,9 @@
 import React from "react";
-// import useStyles from "../components/UserAuth/styles";
-//  import { Avatar, Typography } from "@material-ui/core";
-//  import { Button } from "react-bootstrap";
- import { Link } from "react-router-dom";
+
+import { Link } from "react-router-dom";
 import "../style/Header.css";
 
 function Header() {
-  // const classes = useStyles();
-  // const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
-  // console.log(user);
-  // useEffect(() => {
-  //   // const token = user?.token;
-
-  //   setUser(JSON.parse(localStorage.getItem("profile")));
-  // }, []);
   return (
     <nav className="header">
       <Link to="/" className="header-logo">
@@ -33,46 +23,11 @@ function Header() {
             </Link>
           </li>
 
-          {/* <>
-            {user ? (
-              <div className="profile">
-                <Avatar
-                  className="avatar"
-                  alt={user.result.name}
-                  src={user.result.imageUrl}
-                >
-                   {user.result.name.CharAt(0)};
-                </Avatar>
-                <Typography className="name" variant="h5">
-                  {user.result.name}
-                </Typography>
-                <Button
-                  variant="contained"
-                  className="logoutbutton"
-                  color="blue"
-                >
-                  Logout
-                </Button>
-              </div>
-            ) : (
-              <li className="header-list">
-                <Link to="/auth" className="header-link">
-                  <span>Login</span>
-                </Link>
-              </li>
-            )}
-          </> */}
-           <li className="header-list">
+          <li className="header-list">
             <Link to="/auth" className="header-link">
               <span>Login</span>
             </Link>
           </li>
-
-          {/* <li className="header-list">
-            <Link to="signup" className="header-link">
-              <span>SignUp</span>
-            </Link>
-          </li>  */}
         </ul>
       </div>
     </nav>
